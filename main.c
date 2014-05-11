ad26b3b979e21515ff0c96a2d7c8c0224395781b
igutekunst <igutekunst@gmail.com>
2014-05-11 18:39:09 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,10 +5,12 @@ void add_one(ComputationNode * self) {
   self->value = getIntDep(self, 0) + 1;
   printf("Evalutated. adOne = %d\n", self->value);
 }
+
+
 int main(int argc, char * argv[]) {
-  printf("OMG BITCHES\r\n");
   ComputationNode * x = Int(5);
   ComputationNode * c = create_computation("AddOne", add_one, x,  Int(7), End());
+  
   int * v = (int *) evaluate(c);
   printf("Value: %d\n", *v);
   v = (int *) evaluate(c);
